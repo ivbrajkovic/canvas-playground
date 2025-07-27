ca02d9cbc9da381b6d8e665a2fd4e3d050ad242d
Ivan Brajkovic <ivan.brajkovic@icloud.com>
2025-07-27 11:20:10 +0000

    refactor: update Skeleton and Tooltip components for improved structure and styling
    - Refactored Skeleton component to use data-slot attribute and updated class names.
    - Enhanced Tooltip component with better structure, added TooltipProvider, and improved TooltipContent with a portal.
    - Introduced useIsMobile hook for mobile detection.
    - Removed outdated Tailwind CSS configuration file.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,45 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-
 import './globals.css';
 
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import { AppShell } from '@/components/app-shell';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
+	src: './fonts/GeistVF.woff',
+	variable: '--font-geist-sans',
+	weight: '100 900',
 });
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+	src: './fonts/GeistMonoVF.woff',
+	variable: '--font-geist-mono',
+	weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: 'Canvas Playground',
-  description: '2024 @ Ivan Brajković - Canvas Playground',
+	title: 'Canvas Playground',
+	description: '2024 @ Ivan Brajković - Canvas Playground',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} dark flex min-h-screen flex-col antialiased`}
-        className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
-      >
-        <AppShell>{children}</AppShell>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<AppShell>{children}</AppShell>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
